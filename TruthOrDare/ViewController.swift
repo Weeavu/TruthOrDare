23dadbbc78d4d00789215a0a83f15602bca7d759
Vu Nguyen <vuvu@vus-macbook-pro.local>
2018-02-07 13:53:04 -0500

    Initial Commit

---
+++ TruthOrDare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TruthOrDare
+//
+//  Created by Vu Nguyen on 2/7/18.
+//  Copyright © 2018 Vu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
